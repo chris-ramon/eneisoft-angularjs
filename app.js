9628e11bb70f04d829c267a72463a4ccfad4b5bf
Christian Ramon <rchristian.ramon@gmail.com>
2013-09-20 02:05:53 -0500

    add basic dependency injection example

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 var myApp = angular.module('myApp', []);
+
+myApp.controller('myController', function($scope,
+                                          productFactory) {
+    $scope.products = productFactory.getProducts();
+});
+
+myApp.factory('productFactory', function() {
+    var factory = {};
+    factory.getProducts = function() {
+        return [
+            {id:1, name: 'First Product'},
+            {id:2, name: 'Second Product'},
+            {id:3, name: 'Third Product'}
+        ]
+    }
+    return factory;
+});
