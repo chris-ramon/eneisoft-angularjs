f889a4c4790153701bfd19e86416604cca068770
Christian Ramon <rchristian.ramon@gmail.com>
2013-09-20 02:58:37 -0500

    adding 4 example

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('myController', function($scope) {
     $scope.texto = "hi from eneisoft!";
-    $scope.evento = function() {
-      alert('cool function !!!');
-    }
 });
 
 myApp.directive('zippy', function() {
   return {
       restrict: 'AE',
       transclude: true,
-      scope: {text: '=', evento: '&'},
+      scope: {text: '='},
       templateUrl: 'zippyTemplate.html',
       link: function(scope) {
           scope.zippyClosed = true;
           scope.toggleZippy = function() {
               scope.zippyClosed = !scope.zippyClosed;
-              scope.evento();
           }
       }
   }
