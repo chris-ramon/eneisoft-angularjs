4cbffce46a5e0d3dc0dc0f0a4be386e0443583a1
Christian Ramon <rchristian.ramon@gmail.com>
2013-09-20 01:58:04 -0500

    directives

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 var myApp = angular.module('myApp', []);
+
+myApp.directive('miDirectiva', function() {
+  return {
+      restrict: 'AE',
+      template: '<h1>Esto es una directiva y también un componente.</h1>'
+  }
+});
