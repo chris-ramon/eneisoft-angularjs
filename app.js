2a9639213ae74233a9e42671fcb69743791e9dd2
Christian Ramon <rchristian.ramon@gmail.com>
2013-09-20 02:14:38 -0500

    upd styles

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ var myApp = angular.module('myApp', []);
 myApp.directive('miDirectiva', function() {
   return {
       restrict: 'AE',
-      template: '<h1>Esto es una directiva y también un componente.</h1>'
+      template: '<h1>Esto es una directiva y un componente.</h1>'
   }
 });
